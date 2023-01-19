ce389d92df87df8fa0a9a610124a16762d0ee7d5
mehmedkucevic <kucevicmehmed7@gmail.com>
2023-01-19 19:37:22 +0100

    ..

---
+++ JavaScript/36.cas/app.js
@@ -0,0 +1,6 @@
+// 1. Iz niza od 10 elemenata izracunati sumu parnih brojeva niza.
+// 2. From the array of numbers, choose even double even numbers and compute the sum using Array's filter, map and reduce methods.
+// 3. Write a JavaScript program to find the most frequent item of an array.
+// Sample array :
+const arr1 = [3, "a", "a", "a", 2, 3, "a", 3, "a", 2, 4, 9, 3];
+// 4. Write a JavaScript program which accept a string as input and swap the case of each character. For example if you input 'The Quick Brown Fox' the output should be 'tHE qUICK bROWN fOX'.

39f7c92fc47c8c88660974438b2574fac930ee01
mehmedkucevic <kucevicmehmed7@gmail.com>
2023-01-31 19:53:07 +0100

    push

--- JavaScript/41.cas/app2.js
+++ JavaScript/41.cas/app2.js
@@ -1,33 +1,33 @@
 // JS objekti su mutable (promenljivi)
 
-const dzenan = {
-  ime: "Dzenan",
-  prezime: "Mecinovic",
-  godine: 19,
-};
+// const dzenan = {
+//   ime: "Dzenan",
+//   prezime: "Mecinovic",
+//   godine: 19,
+// };
 
-const dzenan2 = dzenan;
+// const dzenan2 = dzenan;
 
-console.log(dzenan2);
-dzenan2.prezime = "Kosuta";
-dzenan2.godine = 26;
-console.log(dzenan2);
-console.log(dzenan);
+// console.log(dzenan2);
+// dzenan2.prezime = "Kosuta";
+// dzenan2.godine = 26;
+// console.log(dzenan2);
+// console.log(dzenan);
 
 // brisanje svojstava iz objekta:
 
-const person = {
-  ime: "Mitar",
-  prezime: "Vranic",
-  godine: 20,
-  skola: "Prva Tehnicka",
-};
-delete mitar.skola;
+// const person = {
+//   ime: "Mitar",
+//   prezime: "Vranic",
+//   godine: 20,
+//   skola: "Prva Tehnicka",
+// };
+// delete Mitar.skola;
 // delete obj.property brise i key i value
-console.log(mitar);
+// console.log(mitar);
 
-delete mitar["skola"];
-
+// delete mitar["skola"];
+// 
 // NAPOMENA
 // Vrednosti nekog objekta mogu biti
 // Objekti
@@ -36,14 +36,28 @@ delete mitar["skola"];
 
 // Konvertovanje objekta u niz :
 // Object.values(obj)
-const niz = Object.values(mitar);
-console.log(niz);
+// const niz = Object.values(mitar);
+// console.log(niz);
 
 // dodavanje novih svojstava:
-mitar.punoletnost = true;
-console.log(mitar);
-
-const myCar = {};
+// mitar.punoletnost = true;
+// console.log(mitar);
+// clearTimeout
+const myCar = {
+  id: 1,
+  marka: "Audi",
+  model: "a4",
+  boja: "Crvena",
+  pogon: "prednji",
+  menjac: "automatski",
+  kontakt: [0622222, 02033322],
+  servis: {
+    datum: "04,maj",
+    km: 23000,
+    serviser: "Pasovic",
+  },
+  udaran: true,
+};
 
 myCar.trenutnaBrzina = 0;
 myCar.maksimalnaBrzina = 260;
@@ -73,3 +87,19 @@ myCar.koci= function (){
 myCar.koci()
 console.log(myCar)
 ////
+// Domaci zadatak:
+// // Napraviti objekat sa sledecim svojstvima:
+// 1. firstName,
+// 2. lastName,
+// 3. language,
+// 4. setLanguage - metoda za setovanje jezika,
+// 5. setNickName - metoda (svaka osoba ima nadimak sastavljen od prva dva slova
+// imena i prva dva slova prezimena)
+
+// const moje = {
+//   ime: "Mehmed",
+//   prezime: "Kucevic",
+//   jezik : "srpski"
+  
+ 
+// }

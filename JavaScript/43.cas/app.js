b9b603679a9a9d4f556ff442bca0fdf0d650b45d
mehmedkucevic <kucevicmehmed7@gmail.com>
2023-02-02 20:52:32 +0100

    call/apply/bind/JS

---
+++ JavaScript/43.cas/app.js
@@ -0,0 +1,54 @@
+const amer = {
+  firstName: "Amer",
+  lastName: "Honic",
+  age: 19,
+  fullName: function () {
+    return `${this.firstName} ${this.lastName}`;
+  },
+};
+const emin = {
+  firstName: "Emin",
+  lastName: "Zukanovic",
+  age: 19,
+};
+
+// Pozivanje neke funkcije za odredjeni objekat se moze izvrsiti pomocu funkcija:
+// 1. call ()
+// 2. apply ()
+// 3. bind ()
+
+// Prvo treba pristupiti funkciji, pa onda objasniti na kom se objektu treba primeniti
+
+console.log(amer.fullName.call(emin));
+
+// 2. primer
+
+const restoran1 = {
+  hrana: "Spaghetti",
+  gosti: "veoma zadovolnji",
+  lokacija: "Stevana Nemanje, 64",
+};
+
+const restoran2 = {
+  hrana: "burek",
+  gosti: "zgadjeni",
+  lokacija: "Stanice Spasojevic, 62",
+};
+
+function poruka (mesto, drzava)  {
+  return `Vecerasnji specijalitet u nasem restoranu je bio ${this.hrana}. Nasi gosti su ${this.gosti}, te srecni zbog toga.\n Nalazimo se u ulici ${this.lokacija}, u ${mesto}, ${drzava}.`;
+};
+
+//call () metoda za prvi argument ima objekat na koji se primenjuje a ostali argumenti su argumenti metode koja se izrsava
+
+console.log( poruka.call(restoran1, "Novi Pazar", "Srbija") )
+
+// apply() metoda za prvi argument ima objekat na koji se primenjuje, drugi argument je niz gde smestamo argumente metode koja se izvrsava
+
+console.log(poruka.apply(restoran2,["Sarajevo","Bosna"]))
+
+// bind () zahteva prethocdno definisanje promenljive (funkcije) za odredjeni objekat, pa onda pozivanje funkcije sa eventualnim argumentima
+
+const bindFunction = poruka.bind(restoran1)
+console.log(bindFunction("Novi Sad", "Srbija"))
+
--- JavaScript/43.cas/domaci.js
+++ JavaScript/43.cas/domaci.js
@@ -1,19 +1,19 @@
 // program to sort array by property name
 
-function sortiranje(a, b) {
-  const naslov = a.title.toUpperCase();
-  const autor = b.author.toUpperCase();
-  let comparison = 0;
+// function sortiranje(a, b) {
+//   const naslov = a.title.toUpperCase();
+//   const autor = b.author.toUpperCase();
+//   let comparison = 0;
 
-  if (naslov > autor) {
-    comparison = 1;
-  } else if (naslov < autor) {
-    comparison = -1;
-  }
-  return comparison;
-}
+//   if (naslov > autor) {
+//     comparison = 1;
+//   } else if (naslov < autor) {
+//     comparison = -1;
+//   }
+//   return comparison;
+// }
 
-const students = [
+const library = [
   {
     title: "The Road Ahead",
     author: "Bill Gates",
@@ -30,4 +30,19 @@ const students = [
     libraryID: 3245,
   },
 ];
-console.log(students.sort(sortiranje));
+// console.log(students.sort(sortiranje));
+
+// II nacin
+
+const sort2 = (arr) => {
+  const newArr = arr.map((element) => element.title);
+  newArr.sort();
+  const finalArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr.find((element) => element.title === newArr[i])) {
+      finalArr.push(arr.find((element) => element.title === newArr[i]));
+    }
+  }
+  return finalArr;
+};
+console.log(sort2(library));

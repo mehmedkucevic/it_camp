ae476d98e2a706f0a3d0bea019ac0f05142efc51
mehmedkucevic <kucevicmehmed7@gmail.com>
2023-02-11 19:32:11 +0100

    exercise for exam

--- JavaScript/46.cas/app.js
+++ JavaScript/46.cas/app.js
@@ -1,9 +1,9 @@
 function position(a, b) {
-  const pomocniB = b.filter((element)=> element < a.length);
+  const pomocniB = b.filter((element) => element < a.length);
   const noviArr = [];
 
   for (let i = 0; i < a.length; i++) {
-    noviArr.push(a[pomocniB[i]]) 
+    noviArr.push(a[pomocniB[i]]);
   }
   return noviArr;
 }
@@ -11,4 +11,14 @@ console.log(position([100, 101, 102], [1, 1, 2]));
 
 ////////////////////////////////////////////////////////////////////////////////////
 
+function counter(arr) {
+  let brojac = 0;
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] === 2 * arr[i - 1] && arr[i] === arr[i + 1] - 10) {
+    brojac ++
+    }
+  }
+  return brojac
+}
 
+console.log(counter([10,20,30,60,70]))

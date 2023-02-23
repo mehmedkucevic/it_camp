a04d3e4d74e22b70ff404af7d978562a55c6962b
mehmedkucevic <kucevicmehmed7@gmail.com>
2023-02-23 20:53:49 +0100

    posle testa

---
+++ JavaScript/exercise/app.js
@@ -0,0 +1,15 @@
+// I
+
+function isString (txt){
+    const str = txt.toString()
+    if(txt === str ){
+        return "Poslati argument je string"
+    } else if(txt !== str ){
+return "Poslati argument nije string"
+    }
+}
+console.log(isString(12))
+console.log(isString("string"))
+console.log(isString([1,2,"secer"]))
+
+// II
